b077a7006039999c5d44e8d32bfe112498dcf985
Leo Lai <leo@Leos-MacBook-Air.local>
2019-12-08 20:07:50 -0500

    init commit

---
+++ src/store/configureStore.js
@@ -0,0 +1,18 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import authReducer from '../reducers/auth';
+import thunk from 'redux-thunk';
+
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export default () => {
+    // Store Creation
+    const store = createStore(
+        combineReducers({
+            auth: authReducer
+        }),
+        composeEnhancers(applyMiddleware(thunk))
+    );
+    return store;
+};
+
